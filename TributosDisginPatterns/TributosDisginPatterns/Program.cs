8998ce6f1d0c68d436f7822022d36e875dcadb4c
Yan Paiva <yan.andrade@mereo.com>
2022-06-29 16:38:34 -0300

    Cria projeto console.

---
+++ TributosDisginPatterns/TributosDisginPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TributosDisginPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
